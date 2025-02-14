551b3a4965fbaaddab4347abc80985f1c203ff17
Jason Hao <jasonhao518@gmail.com>
2025-02-14 23:24:25 +0100

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -5,13 +5,8 @@ import Resend from "next-auth/providers/resend";
 import { env } from "@/env.mjs";
 import { sendVerificationRequest } from "@/lib/email";
 import Passkey from "next-auth/providers/passkey"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
- 
-const prisma = new PrismaClient()
 
 export default {
-  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       clientId: env.GOOGLE_CLIENT_ID,
