4d9198b2f819540fb5129bfda76200187094e821
Jason Hao <jasonhao518@gmail.com>
2025-02-14 23:09:44 +0100

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,6 @@
 import type { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/nodemailer";
+import EmailProvider from "next-auth/providers/email";
 
 import { env } from "@/env.mjs";
 import { sendVerificationRequest } from "@/lib/email";
@@ -16,9 +16,9 @@ export default {
         host: "email-smtp.us-east-1.amazonaws.com",
         port: 587,
         auth: {
-          user: process.env.SMTP_USERNAME, // Set this in .env
-          pass: process.env.SMTP_PASSWORD, // Set this in .env
-        },
+          user: process.env.SMTP_USERNAME,
+          pass: process.env.SMTP_PASSWORD
+        }
       },
       from: process.env.EMAIL_FROM
     }),
