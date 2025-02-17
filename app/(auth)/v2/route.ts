38594cfa992df25b3b13135efa08b39b3e594531
Jason Hao <jasonhao518@gmail.com>
2025-02-17 17:37:54 +0100

    Update route.ts

--- app/(auth)/v2/route.ts
+++ app/(auth)/v2/route.ts
@@ -1,4 +1,5 @@
 // app/api/v2/[...slug]/route.js
+import { prisma } from '@/lib/db';
 import { NextRequest, NextResponse } from 'next/server';
 export async function GET(req: NextRequest) {
     // Return 401 Unauthorized with required headers
@@ -8,17 +9,27 @@ export async function GET(req: NextRequest) {
     }
     console.log(authHeader);
     if (authHeader && authHeader.startsWith('Basic ')) {
-      //const parsed = await verifyToken(authHeader.substring(7));
-      const isExpired = false;
-      if(!isExpired) {
-        return NextResponse.json({
-          'content-type': 'application/json',
-          'docker-distribution-api-version': 'registry/2.0',
-        });
-      }else {
+      const base64Credentials = authHeader.substring(6);
+      const decoded = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+      const [username, password] = decoded.split(':');
+
+      if (!username || !password) {
+        return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
+      }
+
+      // Query database using Prisma
+      const user = await prisma.k8sCluster.findUnique({
+        where: { id: username  },
+      });
+
+      if (!user || user.apiKey !== password) {
         return NextResponse.json({ message: 'Authentication failed' }, { status: 401 });
       }
 
+      return NextResponse.json({
+        'content-type': 'application/json',
+        'docker-distribution-api-version': 'registry/2.0',
+      });
     }
 
     return new NextResponse("Unauthorized", {
