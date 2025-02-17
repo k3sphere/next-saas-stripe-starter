ca53c62a470f25426707109e0504637a042cd672
Jason Hao <jasonhao518@gmail.com>
2025-02-17 17:26:49 +0100

    Update route.ts

--- app/(auth)/v2/route.ts
+++ app/(auth)/v2/route.ts
@@ -26,6 +26,7 @@ export async function GET(req: NextRequest) {
         headers: {
             "Content-Type": "application/json",
             "Docker-Distribution-API-Version": "registry/2.0",
+            "WWW-Authenticate": 'Basic realm="Docker Registry"',
         },
     });
 }
