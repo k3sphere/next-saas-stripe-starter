b69616de224b5b193f0883e669ae78992a701c73
Jason Hao <jasonhao518@gmail.com>
2025-02-17 18:34:39 +0100

    Update route.ts

--- app/api/cluster/route.ts
+++ app/api/cluster/route.ts
@@ -2,6 +2,20 @@ import { auth } from "@/auth";
 
 import { prisma } from "@/lib/db";
 
+import { randomBytes } from 'crypto';
+
+function generateClientSecret(length: number = 32): string {
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+  const randomBytesArray = randomBytes(length);
+  let secret = '';
+
+  for (let i = 0; i < length; i++) {
+    secret += charset[randomBytesArray[i] % charset.length];
+  }
+
+  return secret;
+}
+
 export const GET = auth(async (req) => {
   if (!req.auth) {
     return new Response("Not authenticated", { status: 401 });
@@ -39,8 +53,10 @@ export const POST = auth(async (req) => {
   const { name, location } =
   await req.json();
   console.log(name, location)
+  const apiKey = generateClientSecret(32)
+  
   try {
-    const clusters = await prisma.k8sCluster.create({ data: { userId, name, location, apiKey: "something"}});
+    const clusters = await prisma.k8sCluster.create({ data: { userId, name, location, apiKey}});
     return new Response(JSON.stringify(clusters), { status: 200 });
   } catch (error) {
     console.log(error)
