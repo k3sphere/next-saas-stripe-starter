7cacd3be0fc631efe02d4a85e07f1273462a8fac
Jason Hao <jasonhao518@gmail.com>
2025-02-18 19:46:31 +0100

    Update cluster-info.tsx

--- components/k8s/cluster-info.tsx
+++ components/k8s/cluster-info.tsx
@@ -36,7 +36,7 @@ export function ClusterInfo() {
         <TableCell className="font-medium">
           <Link
             target="_blank"
-            href={`https://app.k3sphere.com/clusters/${String(cluster?.id)}`}
+            href={`https://app.k3sphere.com/c/${String(cluster?.id)}`}
             className="font-semibold hover:underline"
           >
             {cluster?.name}
