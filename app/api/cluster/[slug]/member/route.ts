de7c50e703f7a721da68c2b1c3ae2c4b41b2b067
Jason Hao <jasonhao518@gmail.com>
2025-03-04 23:24:22 +0100

    add member api

--- app/api/cluster/[slug]/member/route.ts
+++ app/api/cluster/[slug]/member/route.ts
@@ -53,11 +53,37 @@ export const POST = auth(async (req) => {
     return new Response("Invalid user", { status: 401 });
   }
   const userId = currentUser.id
-  const { name, location } =
+  const { email, name,role } =
   await req.json();
-  console.log(name, location)
+  console.log(email, name,role)
+  const { pathname } = req.nextUrl;
+  const segments = pathname.split("/")
+  const clusterId = segments[segments.length-2]; // Get the last segment
+
   try {
-    const clusters = await prisma.k8sCluster.create({ data: { userId, name, location}});
+
+    const member = await prisma.member.findFirst({
+      where: {
+        userId: currentUser.id,
+        clusterId: clusterId,
+        delete: false
+      },
+      select: {
+        role: true,
+        cluster: {
+          select: {
+            id: true,
+            name: true,
+            location: true,
+            publicKey: true,
+          }
+        },
+      },
+    });
+    if (member == null || member.role !== "OWNER") {
+      return new Response("No access", { status: 401 });
+    }
+    const clusters = await prisma.member.create({ data: { email,name,role,clusterId}});
     return new Response(JSON.stringify(clusters), { status: 200 });
   } catch (error) {
     console.log(error)
--- app/api/cluster/[slug]/node/route.ts
+++ app/api/cluster/[slug]/node/route.ts
@@ -32,26 +32,3 @@ export const GET = auth(async (req) => {
 
 });
 
-
-export const POST = auth(async (req) => {
-  if (!req.auth) {
-    return new Response("Not authenticated", { status: 401 });
-  }
-
-  const currentUser = req.auth.user;
-  if (!currentUser || !currentUser.id) {
-    return new Response("Invalid user", { status: 401 });
-  }
-  const userId = currentUser.id
-  const { name, location } =
-  await req.json();
-  console.log(name, location)
-  try {
-    const clusters = await prisma.k8sCluster.create({ data: { userId, name, location}});
-    return new Response(JSON.stringify(clusters), { status: 200 });
-  } catch (error) {
-    console.log(error)
-    return new Response("Internal server error", { status: 500 });
-  }
-
-});
