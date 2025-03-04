9dc9fd197c2f3b202ca7a9e00f173cd55dbf07e2
Jason Hao <jasonhao518@gmail.com>
2025-03-04 09:39:39 +0100

    fix github link

--- components/sections/hero-landing.tsx
+++ components/sections/hero-landing.tsx
@@ -8,7 +8,7 @@ import { Icons } from "@/components/shared/icons";
 
 export default async function HeroLanding() {
   const { stargazers_count: stars } = await fetch(
-    "https://api.github.com/repos/mickasmt/next-saas-stripe-starter",
+    "https://api.github.com/repos/k3sphere/podman",
     {
       ...(env.GITHUB_OAUTH_TOKEN && {
         headers: {
