cbfe2056c9be0191238c44d10ded0ee11dd65412
Jason Hao <jasonhao518@gmail.com>
2025-03-06 17:01:13 +0100

    add routing table

--- app/api/cluster/route.ts
+++ app/api/cluster/route.ts
@@ -72,7 +72,7 @@ export const POST = auth(async (req) => {
     const clusters = await prisma.k8sCluster.create({ data: { userId, name, location, apiKey}});
     const relays = await prisma.relayServer.findMany({ where: { location}});
     await prisma.clusterRelay.createMany({ data: relays.map((relay) => ({ clusterId: clusters.id, relayId: relay.id}))}) 
-    await prisma.member.create({ data: { userId, clusterId: clusters.id, role: "OWNER", name: currentUser.name}}) 
+    await prisma.member.create({ data: { userId, clusterId: clusters.id, role: "OWNER", name: currentUser.name,synched:true}}) 
     return new Response(JSON.stringify(clusters), { status: 200 });
   } catch (error) {
     console.log(error)
